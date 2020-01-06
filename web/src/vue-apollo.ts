64b38eead579c57f60bbcb22d3324d8c8be9c1aa
Elgin Beloy <beloy.elgin@gmail.com>
2020-01-05 21:01:28 -0800

    Updated URL for backend.

--- web/src/vue-apollo.ts
+++ web/src/vue-apollo.ts
@@ -6,7 +6,7 @@ import VueApollo from "vue-apollo";
 // HTTP connection to the API
 const httpLink = createHttpLink({
   // You should use an absolute URL here
-  uri: "https://dazzling-morse-001506.netlify.com/graphql"
+  uri: "https://storr-backend.herokuapp.com/graphql"
 });
 
 // Cache implementation
